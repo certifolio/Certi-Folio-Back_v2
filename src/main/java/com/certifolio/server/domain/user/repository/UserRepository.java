4ee4d25afce30fe71efabf455d255b32408f36b3
Jonghun Lim <jhlim00812@gmail.com>
2026-04-03 17:06:47 +0900

    [feat] UserRepository 추가

---
+++ src/main/java/com/certifolio/server/domain/user/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.certifolio.server.domain.user.repository;
+
+import com.certifolio.server.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByProviderAndProviderId(String provider, String providerId);
+}
+
