7292bea8248b5f350458acfc5b11f6062211dfb3
Jonghun Lim <jhlim00812@gmail.com>
2026-04-07 00:19:49 +0900

    [feat] ObjectMapper 빈 추가 및 스케줄링 활성화 annotation 추가

--- src/main/java/com/certifolio/server/global/config/WebClientConfig.java
+++ src/main/java/com/certifolio/server/global/config/WebClientConfig.java
@@ -24,4 +24,9 @@ public class WebClientConfig {
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
 }
