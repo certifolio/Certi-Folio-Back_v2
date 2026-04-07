2eee34e96c7ae8fd8fa6c994c46a4467815b2ea8
Jonghun Lim <jhlim00812@gmail.com>
2026-04-07 17:17:00 +0900

    [feat] Community domain 엔티티 추가 - Post 및 Comment

---
+++ src/main/java/com/certifolio/server/domain/community/entity/Comment.java
@@ -0,0 +1,42 @@
+package com.certifolio.server.domain.community.entity;
+
+import com.certifolio.server.domain.user.entity.User;
+import com.certifolio.server.global.common.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "comments")
+public class Comment extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Builder
+    public Comment(Post post, User user, String content) {
+        this.post = post;
+        this.user = user;
+        this.content = content;
+    }
+
+    public void update(String content) {
+        this.content = content;
+    }
+}
